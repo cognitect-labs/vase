2e1ab9d626cef2c3165314b9e3036f076c3ea009
Michael Nygard <mtnygard@gmail.com>
2016-05-03 13:45:28 -0400

    Tests should verify literal printing.

--- test/vase/literals_test.clj
+++ test/vase/literals_test.clj
@@ -3,7 +3,8 @@
             [vase.literals :as lit]
             [io.pedestal.test :refer :all]
             [vase.test-helper :as helper]
-            [vase.actions :as actions]))
+            [vase.actions :as actions]
+            [clojure.string :as str]))
 
 (deftest test-data-massging
   "This test ensures that data received can be transformed into a form
@@ -29,3 +30,23 @@
                                :name "paul"})]
     (is (= (:status response) 200))
     (is (empty? (helper/response-data response)))))
+
+(defn- reader-literal [a]
+  (first (str/split (pr-str a) #"\{")))
+
+(deftest printing-literals
+  (testing "actions print as reader literals"
+    (are [word action] (= word (reader-literal action))
+      "#vase/respond"  (lit/respond  {:name :responder})
+      "#vase/redirect" (lit/redirect {:name :redirect :url ""})
+      "#vase/validate" (lit/validate {:name :validate})
+      "#vase/query"    (lit/query    {:name :query :query []})
+      "#vase/transact" (lit/transact {:name :transact})))
+
+  (testing "actions round-trip through the reader"
+    (are [action] (= action (read-string (pr-str action)))
+      (lit/respond  {:name :responder :bar :baz :extra "stuff"})
+      (lit/redirect {:name :redirect :url ""})
+      (lit/validate {:name :validate})
+      (lit/query    {:name :query :query []})
+      (lit/transact {:name :transact}))))
