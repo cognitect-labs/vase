7ee9eeb27b4736258909c79029484763a38e52f7
Daniel De Aguiar <ddeaguiar@pointslope.com>
2016-06-17 13:16:39 -0400

    Introduced specs for the Vase specification.
    Also bumped dependency versions.

--- project.clj
+++ project.clj
@@ -2,27 +2,27 @@
   :description "Vase: Pedestal API Container"
   :url "https://github.com/relevance/vase"
   :dependencies [;; Platform
-                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojure "1.9.0-alpha7"]
 
                  ;; Datomic
-                 [com.datomic/datomic-free "0.9.5350" :exclusions [[com.fasterxml.jackson.core/jackson-core]
+                 [com.datomic/datomic-free "0.9.5372" :exclusions [[com.fasterxml.jackson.core/jackson-core]
                                                                    [com.fasterxml.jackson.core/jackson-databind]
                                                                    [joda-time]]]
                  [io.rkn/conformity "0.4.0" :exclusions [com.datomic/datomic-free]]
 
                  ;; Pedestal
-                 [io.pedestal/pedestal.service "0.4.2-SNAPSHOT" :exclusions [[com.fasterxml.jackson.core/jackson-core]
+                 [io.pedestal/pedestal.service "0.5.0" :exclusions [[com.fasterxml.jackson.core/jackson-core]
                                                                     [com.fasterxml.jackson.core/jackson-databind]
                                                                     [com.fasterxml.jackson.datatype/jackson-datatype-json-org]]]
                  [ohpauleez/themis "0.1.1"]
 
                  ;; Cleanup
                  [commons-codec "1.10"]
-                 [joda-time "2.9.2"]
+                 [joda-time "2.9.4"]
                  [com.fasterxml.jackson.core/jackson-core "2.7.1"]
                  [com.fasterxml.jackson.core/jackson-databind "2.7.1-1"]
                  [com.fasterxml.jackson.datatype/jackson-datatype-json-org "2.7.1"]
-                 [cheshire "5.5.0" :exclusions [[com.fasterxml.jackson.core/jackson-core]]]]
+                 [cheshire "5.6.1" :exclusions [[com.fasterxml.jackson.core/jackson-core]]]]
   :profiles {:dev {:source-paths ["dev"]
                    :resource-paths ["config"
                                     "resources"
@@ -30,16 +30,17 @@
 
                    :dependencies [[org.clojure/tools.trace "0.7.9"]
                                   [org.clojure/tools.namespace "0.2.10"]
-                                  [io.pedestal/pedestal.jetty "0.4.2-SNAPSHOT"]]}
+                                  [io.pedestal/pedestal.jetty "0.5.0"]
+                                  [org.clojure/test.check "0.9.0"]]}
              :test {:dependencies [[org.clojure/test.check "0.9.0"]
                                    ;; Logging
-                                   [org.slf4j/slf4j-api "1.7.16"]
-                                   [ch.qos.logback/logback-classic "1.1.5" :exclusions [[org.slf4j/slf4j-api]]]
+                                   [org.slf4j/slf4j-api "1.7.21"]
+                                   [ch.qos.logback/logback-classic "1.1.7" :exclusions [[org.slf4j/slf4j-api]]]
                                    ;[net.openhft/chronicle-logger-logback "1.1.0" :exclusions [[org.slf4j/slf4j-api]]]
-                                   [org.slf4j/jul-to-slf4j "1.7.16"]
-                                   [org.slf4j/jcl-over-slf4j "1.7.16"]
-                                   [org.slf4j/log4j-over-slf4j "1.7.16"]
-                                   [io.pedestal/pedestal.service-tools "0.4.2-SNAPSHOT" :exclusions [[org.slf4j/log4j-over-slf4j]
+                                   [org.slf4j/jul-to-slf4j "1.7.21"]
+                                   [org.slf4j/jcl-over-slf4j "1.7.21"]
+                                   [org.slf4j/log4j-over-slf4j "1.7.21"]
+                                   [io.pedestal/pedestal.service-tools "0.5.0" :exclusions [[org.slf4j/log4j-over-slf4j]
                                                                                             [org.slf4j/jul-to-slf4j]
                                                                                             [org.slf4j/jcl-over-slf4j]]]]
                     :resource-paths ["resources"
---
+++ src/vase/spec.clj
@@ -0,0 +1,65 @@
+(ns vase.spec
+  "Contains the clojure.spec definitions for the Vase
+   application specification."
+  (:require [io.pedestal.interceptor :as interceptor]
+            [clojure.spec :as s]
+            [clojure.spec.gen :as gen]))
+
+;; -- Predicates --
+(defn valid-uri?
+  "Returns true if v is a non-empty string representation of a uri."
+  [v]
+  (try
+    (boolean (and (string? v) (not-empty v) (java.net.URI. v)))
+    (catch java.net.URISyntaxException e false)))
+
+;; -- Pedestal-specs --
+(s/def ::interceptor #(satisfies? interceptor/IntoInterceptor %))
+(s/def ::route-table-route (s/cat :path valid-uri?
+                            :verb #{:any :get :put :post :delete :patch :options :head}
+                            :handler (s/alt :fn fn? :interceptors (s/coll-of ::interceptor []))
+                            :route-name (s/? (s/cat :_ #(= :route-name %) :name keyword?))
+                            :constraints (s/? (s/cat :_ #(= :constraints %)
+                                                     :constraints (s/map-of keyword?
+                                                                            #(instance? java.util.regex.Pattern %))))))
+(s/def ::route-table (s/* (s/spec ::route-table-route)))
+
+;; -- Vase specs --
+(s/def ::app-name keyword?)
+(s/def ::version  keyword?)
+(s/def ::datomic-uri (s/with-gen (s/and string? valid-uri? #(.startsWith % "datomic"))
+                       #(gen/return (str "datomic:mem://" (java.util.UUID/randomUUID)))))
+
+;; -- descriptor api version specs --
+(s/def ::schemas (s/* qualified-keyword?))
+(s/def ::forward-headers (s/* (s/and string? not-empty)))
+
+;; -- routes --
+(s/def ::get ::interceptor)
+(s/def ::put ::interceptor)
+(s/def ::post ::interceptor)
+(s/def ::delete ::interceptor)
+(s/def ::head ::interceptor)
+(s/def ::options ::interceptor)
+
+(s/def ::action (s/and (s/keys ::opt-un [::get ::put ::post ::delete ::head ::options])
+                       #(not-empty (select-keys % [:get :put :post :delete :head :options]))))
+(s/def ::route (s/cat :path valid-uri? :actions ::action))
+(s/def ::routes (s/* (s/spec ::route)))
+
+(s/def ::api-version (s/keys :req-un [::routes] :opt-un [::schemas ::forward-headers]))
+
+;; -- descriptor app specs --
+;; -- norms --
+(s/def ::tx (s/* (s/or :_ vector? :_ map?)))
+(s/def ::txes (s/* (s/spec ::tx)))
+(s/def ::requires (s/* qualified-keyword?))
+(s/def ::norms (s/map-of qualified-keyword? (s/keys ::req-un [::txes] ::opt-un [::requires])))
+
+(s/def ::app (s/and (s/map-of keyword? (s/or :api-version ::api-version :norms ::norms)) (s/keys :req-un [::norms])))
+
+;; -- The descriptor spec --
+(s/def ::descriptor (s/and (s/map-of ::app-name ::app) not-empty))
+
+;; -- Vase spec --
+(s/def ::spec (s/keys :req-un [::app-name ::version ::descriptor ::datomic-uri]))
