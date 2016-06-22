4389ebdafdcbbde0377279ed14c07f2b5f9dac29
Yoko Harada <yokolet@gmail.com>
2016-06-22 14:52:00 -0400

    To avoid datomic pro dependency problem, changed to use free version provided by vase

--- samples/pet-store/project.clj
+++ samples/pet-store/project.clj
@@ -10,10 +10,7 @@
                  [org.slf4j/jul-to-slf4j "1.7.21"]
                  [org.slf4j/jcl-over-slf4j "1.7.21"]
                  [org.slf4j/log4j-over-slf4j "1.7.21"]
-                 [com.datomic/datomic-pro "0.9.5372" :exclusions [[com.fasterxml.jackson.core/jackson-core]
-                                                                  [com.fasterxml.jackson.core/jackson-databind]
-                                                                  [joda-time]]]
-                 [com.cognitect/vase "0.1.0-SNAPSHOT" :exclusions [com.datomic/datomic-free]]]
+                 [com.cognitect/vase "0.1.0-SNAPSHOT"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "pet-store.server/run-dev"]}
