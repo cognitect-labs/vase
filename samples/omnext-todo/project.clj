a20c9610055ac7ec4100d7eca6b8e8e2d16a58fd
Paul deGrandis <paul.degrandis@gmail.com>
2017-01-31 10:14:29 -0500

    Update the Vase version in README/Samples

--- samples/omnext-todo/project.clj
+++ samples/omnext-todo/project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [io.pedestal/pedestal.service "0.5.2"]
-                 [com.cognitect/pedestal.vase "0.9.0-SNAPSHOT"]
+                 [com.cognitect/pedestal.vase "0.9.0"]
 
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
--- samples/pet-store/project.clj
+++ samples/pet-store/project.clj
@@ -3,14 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [io.pedestal/pedestal.service "0.5.1"]
                  [io.pedestal/pedestal.jetty "0.5.1"]
                  [ch.qos.logback/logback-classic "1.1.7" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.21"]
                  [org.slf4j/jcl-over-slf4j "1.7.21"]
                  [org.slf4j/log4j-over-slf4j "1.7.21"]
-                 [com.cognitect/pedestal.vase "0.9.0-SNAPSHOT"]]
+                 [com.cognitect/pedestal.vase "0.9.0"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "pet-store.server/run-dev"]}
