b055923113cd7b57ce5f3bc5f1effd58ed088650
Michael Nygard <mtnygard@gmail.com>
2017-01-31 13:20:09 -0600

    Apply namespace to builtin interceptors.

--- src/com/cognitect/vase/interceptor.clj
+++ src/com/cognitect/vase/interceptor.clj
@@ -9,13 +9,13 @@
 
 (def attach-received-time
   (i/-interceptor
-   {:name  :attach-received-time
+   {:name  ::attach-received-time
     :doc   "Attaches a timestamp to every request."
     :enter (fn [ctx] (assoc-in ctx [:request :received-time] (clj-time/now)))}))
 
 (def attach-request-id
   (i/-interceptor
-   {:name  :attach-request-id
+   {:name  ::attach-request-id
     :doc   "Attaches a request ID to every request;
             If there's a 'request_id' header, it will use that value, otherwise it will generate a short hash"
     :enter (fn [{:keys [request] :as ctx}]
@@ -27,7 +27,7 @@
 (defn forward-headers
   [headers]
   (i/-interceptor
-   {:name  :forward-headers
+   {:name  ::forward-headers
     :doc   "Given an interceptor name and list of headers to forward,
             return an interceptor that attaches those headers to reponses IFF
             they are in the request"
