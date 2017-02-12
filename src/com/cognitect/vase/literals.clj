4efd72838b6794d815f0c26fba2a4890b9489310
Malte <malte.nottmeyer@gmail.com>
2017-02-12 04:14:53 +0100

    Add :db/isComponent to schema-tx reader literal.

--- src/com/cognitect/vase/literals.clj
+++ src/com/cognitect/vase/literals.clj
@@ -11,12 +11,21 @@
 
 ;; Schema literals
 ;; ---------------
-(def accepted-schema-toggles #{:unique :index :fulltext :identity nil})
+(def accepted-schema-toggles #{:unique :index :fulltext :identity :component nil})
+
+(def opt-toggles
+  {:unique    {:db/unique :db.unique/value}
+   :identity  {:db/unique :db.unique/identity}
+   :index     {:db/index true}
+   :fulltext  {:db/fulltext true
+               :db/index true}
+   :component {:db/isComponent true}})
 
 (defn parse-schema-vec [s-vec]
   (let [doc-string (last s-vec)
-        [ident card kind opt-toggle] (butlast s-vec)]
-    (if (contains? accepted-schema-toggles opt-toggle)
+        [ident card kind opt-toggle sec-opt-toggle] (butlast s-vec)]
+    (if (and (contains? accepted-schema-toggles opt-toggle)
+             (contains? accepted-schema-toggles sec-opt-toggle))
       (merge {:db/id (d/tempid :db.part/db)
               :db/ident ident
               :db/valueType (keyword "db.type" (name kind))
@@ -24,13 +33,8 @@
               :db.install/_attribute :db.part/db}
              (when (string? doc-string)
               {:db/doc (str doc-string)})
-             (case opt-toggle
-               :unique   {:db/unique :db.unique/value}
-               :identity {:db/unique :db.unique/identity}
-               :index    {:db/index true}
-               :fulltext {:db/fulltext true
-                          :db/index true}
-               nil))
+             (opt-toggles opt-toggle)
+             (opt-toggles sec-opt-toggle))
       (throw (ex-info (str "Short schema toggles must be one of: " accepted-schema-toggles)
                       {:found-toggle opt-toggle})))))
 
