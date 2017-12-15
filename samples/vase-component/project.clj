c93eb094736206a965eab328c770d429b3fbf27b
Michael Nygard <mtnygard@gmail.com>
2017-12-15 17:38:28 -0600

    Make the vase-component sample work with Clojure 1.9

--- samples/vase-component/project.clj
+++ samples/vase-component/project.clj
@@ -8,18 +8,17 @@
 ; You must not remove this notice, or any other, from this software.
 
 (defproject com.cognitect/vase-component "0.9.3-SNAPSHOT"
-  :description "Sample application using Component library to set up system"
-  :url "https://github.com/cognitect-labs/vase"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
-                 [io.pedestal/pedestal.service "0.5.3"]
-                 [io.pedestal/pedestal.jetty "0.5.3"]
-                 [com.cognitect/pedestal.vase "0.9.3-SNAPSHOT"]
-                 [com.stuartsierra/component "0.3.2"]]
-  :main ^:skip-aot vase-component.main
+  :description    "Sample application using Component library to set up system"
+  :url            "https://github.com/cognitect-labs/vase"
+  :license        {:name "Eclipse Public License"
+                   :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies   [[org.clojure/clojure          "1.9.0"]
+                   [io.pedestal/pedestal.service "0.5.3"]
+                   [io.pedestal/pedestal.jetty   "0.5.3"]
+                   [com.cognitect/pedestal.vase  "0.9.3-SNAPSHOT"]
+                   [com.stuartsierra/component   "0.3.2"]]
   :resource-paths ["config", "resources"]
-  :profiles {:dev {:dependencies [[com.stuartsierra/component.repl "0.2.0"]
-                                  [org.clojure/tools.reader "1.1.0"]
-                                  [org.clojure/tools.namespace "0.3.0-alpha3"]]
-                   :source-paths ["dev"]}})
+  :profiles       {:dev {:dependencies [[com.stuartsierra/component.repl "0.2.0"]
+                                        [org.clojure/tools.reader "1.1.1"]
+                                        [org.clojure/tools.namespace "0.3.0-alpha3"]]
+                         :source-paths ["dev"]}})
