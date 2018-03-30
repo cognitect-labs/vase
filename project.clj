9c6e4bc3a69475753866a722ae0cdf037daee5d8
Michael Nygard <mtnygard@gmail.com>
2018-03-30 08:15:28 -0500

    Version 0.9.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.cognitect/pedestal.vase "0.9.3-SNAPSHOT"
+(defproject com.cognitect/pedestal.vase "0.9.3"
   :description "Vase: Pedestal API Container"
   :url "https://github.com/cognitect-labs/pedestal.vase"
   :dependencies [;; Platform
