6785e5e5ffed39f388a0e8715341d9171f605811
Michael Nygard <mtnygard@gmail.com>
2018-09-23 11:03:04 -0500

    Resolve dependency conflict between Pedestal and Datomic

--- project.clj
+++ project.clj
@@ -6,13 +6,19 @@
 
                  ;; Datomic
                  [com.datomic/datomic-free "0.9.5697" :exclusions [[org.slf4j/slf4j-api]
-                                                                   [org.slf4j/slf4j-nop]]]
+                                                                   [org.slf4j/slf4j-nop]
+                                                                   [org.eclipse.jetty/jetty-util]
+                                                                   [org.eclipse.jetty/jetty-client]]]
                  [com.datomic/client-cloud "0.8.63"]
                  [io.rkn/conformity "0.5.1" :exclusions [com.datomic/datomic-free]]
 
                  ;; Pedestal
                  [io.pedestal/pedestal.service "0.5.4"]
                  [io.pedestal/pedestal.jetty "0.5.4"]
+
+                 ;; Pin Jetty versions to avoid conflict between Datomic and Pedestal
+                 [org.eclipse.jetty/jetty-util "9.4.10.v20180503"]
+                 [org.eclipse.jetty/jetty-client "9.4.10.v20180503"]
 
                  [com.cognitect/fern "0.1.3"]
 
@@ -22,6 +28,7 @@
                  ;; Cleanup
                  [commons-codec "1.11"]
                  [cheshire "5.8.0"]]
+
   :main          ^:skip-aot com.cognitect.vase.main
   :pedantic?     :warn
   :uberjar-name  "vase-standalone.jar"
@@ -31,6 +38,10 @@
                     (if (>= (java.lang.Integer/parseInt major) 9)
                       ["--add-modules" "java.xml.bind"]
                       []))
+  :test-selectors {:default (complement :cloud)
+                   :cloud   :integration
+                   :all     (constantly true)}
+
   :profiles {:srepl {:jvm-opts ^:replace ["-XX:+UseG1GC"
                                           "-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"]}
              :dev {:aliases {"crepl" ["trampoline" "run" "-m" "clojure.main/main"]
