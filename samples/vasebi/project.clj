71c8e2cdc17abb041fdc91d7aebb43d132ea5298
Daniel De Aguiar <ddeaguiar@gmail.com>
2019-04-19 13:54:08 -0400

    Updated Vasebi sample deps.

--- samples/vasebi/project.clj
+++ samples/vasebi/project.clj
@@ -3,15 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
-                 [io.pedestal/pedestal.service "0.5.3"]
-                 [com.cognitect/pedestal.vase "0.9.3-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [io.pedestal/pedestal.service "0.5.5"]
+                 [com.cognitect/pedestal.vase "0.9.3"]
 
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.5.3"]
-                 ;; [io.pedestal/pedestal.immutant "0.5.3"]
-                 ;; [io.pedestal/pedestal.tomcat "0.5.3"]
+                 [io.pedestal/pedestal.jetty "0.5.5"]
+                 ;; [io.pedestal/pedestal.immutant "0.5.5"]
+                 ;; [io.pedestal/pedestal.tomcat "0.5.5"]
 
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.25"]
@@ -22,7 +22,7 @@
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
   ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.3"]]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "vasebi.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.3"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.5"]]}
              :uberjar {:aot [vasebi.server]
                        :main vasebi.server}}
   :main ^{:skip-aot true} vasebi.server)
